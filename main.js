832669b2e27ef12161b7de41cb293a7cf77193ea
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2024-03-10 14:24:36 +0000

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+}
